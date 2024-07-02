c68664ea15e9e4f36bb8dad8bdd3ee55dbd38276
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-07-02 15:06:50 +0200

    add: Evento class fields added

--- src/main/java/org/lessons/java/eventi/Evento.java
+++ src/main/java/org/lessons/java/eventi/Evento.java
@@ -1,5 +1,14 @@
 package org.lessons.java.eventi;
 
+import java.util.Calendar;
+
 public class Evento {
+	
+	//Dichiarazione degli attributi della classe
+	
+	private String titolo;
+	private Calendar dataEvento;
+	private int numPosti;
+	private int numPrenotazioni = 0;
 
 }
