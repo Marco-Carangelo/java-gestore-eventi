ef04450b4e7758999bb37dd2cc13c1f76fe388d7
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-07-02 17:12:52 +0200

    add: main method variables

--- src/main/java/org/lessons/java/eventi/Main.java
+++ src/main/java/org/lessons/java/eventi/Main.java
@@ -1,8 +1,29 @@
 package org.lessons.java.eventi;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//Inizializziamo un'istanza della classe Scanner per poter acquisire input dall'utente
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//Dichiariazione delle variabili
+		
+		String titoloNuovoEvento;
+		Calendar dataNuovoEvento;
+		int postiNuovoEvento;
+		
+		//Variabili per settare la data di dataNuovoEvento
+		
+		int annoNuovoEvento;
+		int meseNuovoEvento;
+		int giornoNuovoEvento;
+		
+		
 		
 		
 		
