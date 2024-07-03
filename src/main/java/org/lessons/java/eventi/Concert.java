1760420760fb7f80e3db6dbbb9eca0a39698b0ec
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-07-03 18:27:38 +0200

    add: Concert attributes getters and setters added

--- src/main/java/org/lessons/java/eventi/Concert.java
+++ src/main/java/org/lessons/java/eventi/Concert.java
@@ -8,9 +8,32 @@ public class Concert extends Event {
 	private LocalTime concertHour;
 	private float concertPrice;
 
-	public Concert(String title, Calendar eventDate, int seatsNumber) {
+	public Concert(String title, Calendar eventDate, int seatsNumber, LocalTime concertHour, float concertPrice) {
 		super(title, eventDate, seatsNumber);
+		
+		this.concertHour = concertHour;
+		this.concertPrice = concertPrice;
 		
 	}
 
+	public LocalTime getConcertHour() {
+		return concertHour;
+	}
+
+	public void setConcertHour(LocalTime concertHour) {
+		this.concertHour = concertHour;
+	}
+
+	public float getConcertPrice() {
+		return concertPrice;
+	}
+
+	public void setConcertPrice(float concertPrice) {
+		this.concertPrice = concertPrice;
+	}
+
+	
+	
+	
 }
+
