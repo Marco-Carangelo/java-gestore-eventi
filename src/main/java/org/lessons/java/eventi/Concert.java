226b9c6092f06b20891e91686e40253e4aeac8da
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-07-03 18:24:13 +0200

    add: Concert class attributes

--- src/main/java/org/lessons/java/eventi/Concert.java
+++ src/main/java/org/lessons/java/eventi/Concert.java
@@ -1,10 +1,12 @@
 package org.lessons.java.eventi;
 
+import java.time.LocalTime;
 import java.util.Calendar;
 
 public class Concert extends Event {
 	
-	
+	private LocalTime concertHour;
+	private float concertPrice;
 
 	public Concert(String title, Calendar eventDate, int seatsNumber) {
 		super(title, eventDate, seatsNumber);
