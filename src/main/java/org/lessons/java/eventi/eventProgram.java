844c8bf4139aca2c167636bf31462221e5a7da45
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-07-05 01:50:16 +0200

    add: class constructor and getters and setters

--- src/main/java/org/lessons/java/eventi/eventProgram.java
+++ src/main/java/org/lessons/java/eventi/eventProgram.java
@@ -6,7 +6,32 @@ import java.util.List;
 public class eventProgram {
 	
 	private String programTitle;
-	private List<Event> eventList;  
+	private List<Event> eventList;
+	
+	public eventProgram(String programTitle) {
+		
+		this.programTitle = programTitle;
+		this.eventList = new ArrayList<Event>();
+		
+	}
+
+	public String getProgramTitle() {
+		return programTitle;
+	}
+
+	public void setProgramTitle(String programTitle) {
+		this.programTitle = programTitle;
+	}
+
+	public List<Event> getEventList() {
+		return eventList;
+	}
+
+	public void setEventList(List<Event> eventList) {
+		this.eventList = eventList;
+	}  
+	
+	
 	
 	
 
